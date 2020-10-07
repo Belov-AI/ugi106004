063bdf2ea6c2daaf72bdc5b1cd904e86fe588b5d
Belov-AI <Belov.Alexander@urfu.ru>
2020-10-07 15:42:59 +0500

    Задание 1

---
+++ Task01/Task01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите первую координату первой точки");
+            var x1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите вторую координату первой точки");
+            var y1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите первую координату второй точки");
+            var x2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите вторую координату второй точки");
+            var y2 = double.Parse(Console.ReadLine());
+
+            var distance = Math.Round(
+                    Math.Sqrt(Math.Pow(x1 - x2, 2) 
+                    + Math.Pow(y1 - y2, 2)), 3);
+
+            Console.WriteLine("Расстояние равно " + distance);
+
+            Console.ReadKey();
+        }
+    }
+}
