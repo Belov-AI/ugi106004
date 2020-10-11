d7cf118f9571f986d8dfb30f54611c03db91504f
Belov-AI <Belov.Alexander@urfu.ru>
2020-10-11 20:15:24 +0500

    Добавлен ранее начатый проект вычисление синуса и косинуса угла

---
+++ SinCos/SinCos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SinCos
+{
+    class Program
+    {
+        static void Main()
+        {
+            PrintSinCos(15);
+            PrintSinCos(37);
+            PrintSinCos(113);
+
+            Console.ReadKey();
+        }
+
+        static void PrintSinCos(double angleInDegrees)
+        {
+            double sin, cos;
+            CalulateRoundedSinCos(angleInDegrees, out sin, out cos);
+
+            Console.WriteLine("sin " + angleInDegrees + "° = " + sin);
+            Console.WriteLine("cos " + angleInDegrees + "° = " + cos);
+            Console.WriteLine();
+        }
+
+        static void CalulateRoundedSinCos(double angleInDegrees, 
+            out double sinus, out double cosinus)
+        {
+            double angleInRadians = DegreesToRadians(angleInDegrees);
+            sinus = Math.Round(Math.Sin(angleInRadians), 3);
+            cosinus = Math.Round(Math.Cos(angleInRadians), 3);
+        }
+
+        static double DegreesToRadians(double angleInDegrees)
+        {
+            return angleInDegrees * Math.PI / 180;
+        }
+
+    }
+}
