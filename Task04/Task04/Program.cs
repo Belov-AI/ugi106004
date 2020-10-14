7632e0c49cfdd4fefc08bb8fac197b79326d82bd
Belov-AI <Belov.Alexander@urfu.ru>
2020-10-14 16:01:37 +0500

    Добавлено 4-е задание

---
+++ Task04/Task04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main()
+        {
+            var x = GetValue();
+
+            Console.WriteLine("x = " + x);
+
+            Console.ReadKey();
+        }
+
+        static double GetValue()
+        {
+            return F(3, 2) * F(2, 4) * F(7, 5);
+        }
+
+        /// <summary>
+        /// Вычисление повторяющегося выражения
+        /// </summary>
+        /// <param name="a">Константа под корнем</param>
+        /// <param name="b">Знаменатель дроби</param>
+        /// <returns></returns>
+        static double F(double a, double b)
+        {
+            return Math.Tan((1 + Math.Sqrt(a)) / b);
+        }
+    }
+}
