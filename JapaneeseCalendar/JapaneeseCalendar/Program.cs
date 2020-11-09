b700e0e33e6d4ae216757900cae5054f8a3f4cca
Belov-AI <Belov.Alexander@urfu.ru>
2020-11-09 13:54:30 +0500

    Доделан JapaneeseCalendar. Добавлен TableOfInchesConversion

--- JapaneeseCalendar/JapaneeseCalendar/Program.cs
+++ JapaneeseCalendar/JapaneeseCalendar/Program.cs
@@ -7,20 +7,27 @@ namespace JapaneeseCalendar
     {
         static void Main()
         {
-            Console.WriteLine("Введите год н. э.");
-            var year = int.Parse(Console.ReadLine());
 
-            var yearInCycle = (year - 1984) % 60;
+            while (true)
+            {
+                Console.WriteLine("Введите год н. э.(Enter - выход)");
+                var input = Console.ReadLine();
 
-            if (yearInCycle < 0)
-                yearInCycle += 60;
+                if (input == "")
+                    break;
 
-            var color = yearInCycle / 12;
-            var animal = yearInCycle % 12;
+                var year = int.Parse(input);
 
-            Console.WriteLine($"{year} — год {GetColor(color)}{GetAnimal(animal)}");
+                var yearInCycle = (year - 1984) % 60;
 
-            Console.ReadKey();
+                if (yearInCycle < 0)
+                    yearInCycle += 60;
+
+                var color = yearInCycle / 12;
+                var animal = yearInCycle % 12;
+
+                Console.WriteLine($"{year} — год {GetColor(color)}{GetAnimal(animal)}\n");
+            }
         }
 
         static string GetColor(int color)
@@ -42,7 +49,33 @@ namespace JapaneeseCalendar
 
         static string GetAnimal(int animal)
         {
-            throw new NotImplementedException();
+            switch (animal)
+            {
+                case 0:
+                    return "й крысы";
+                case 1:
+                    return "й коровы";
+                case 2:
+                    return "го тигра";
+                case 3:
+                    return "го зайца";
+                case 4:
+                    return "го дракона";
+                case 5:
+                    return "й змеи";
+                case 6:
+                    return "й лошади";
+                case 7:
+                    return "й овцы";
+                case 8:
+                    return "й обезьяны";
+                case 9:
+                    return "й курицы";
+                case 10:
+                    return "й собаки";
+                default:
+                    return "й свиньи";
+            }
         }
     }
 }
---
+++ TableOfInchesConversion/TableOfInchesConversion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TableOfInchesConversion
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите первое число в таблице (в дюймах)");
+            var start = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите шаг таблицы (в дюймах)");
+            var step = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество строк в таблице");
+            var numberOfLines = int.Parse(Console.ReadLine());
+
+            //Шапка таблицы
+            Console.WriteLine("\x250C" + new String((char)0x2500, 7) + "\x252C" + new String((char)0x2500, 8) + "\x2510");
+            Console.WriteLine("\x2502 дюймы \x2502   см   \x2502");
+            Console.WriteLine("\x251C" + new String((char)0x2500, 7) + "\x253C" + new String((char)0x2500, 8) + "\x2524");
+
+            var valIninches = start;
+
+            for (var i = 0; i < numberOfLines; i++)
+            {
+                var valInCm = valIninches * 2.54;
+                Console.WriteLine($"\x2502 {valIninches,5:F2} \x2502 {valInCm,6:F2} \x2502");
+                valIninches += step;
+            }
+
+            
+            //Низ таблицы
+            Console.WriteLine("\x2514" + new String((char)0x2500, 7) + "\x2534" + new String((char)0x2500, 8) + "\x2518");
+
+            Console.ReadKey();
+        }
+    }
+}
