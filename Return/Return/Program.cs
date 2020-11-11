a63512ee66875765ea70f0963a18e1bd3f61c87b
Belov-AI <Belov.Alexander@urfu.ru>
2020-11-11 20:55:48 +0500

    Добавлен проект Return Изменения в Logic

---
+++ Return/Return/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Return
+{
+    class Program
+    {
+        static void Main()
+        {
+            var x = DoubleOrTriple(1);
+            PrintInfo(x);
+
+            x = DoubleOrTriple(2);
+            PrintInfo(x);
+
+            Console.ReadKey();
+        }
+
+        static void PrintInfo(int x)
+        {
+            if (x % 2 == 0)
+            {
+                Console.WriteLine($"Число {x} четное");
+                return;
+            }
+                            
+            Console.WriteLine($"Число {x} нечетное");
+        }
+
+        static int DoubleOrTriple(int x)
+        {
+           if(x % 2 != 0)
+                return 2 * x;
+
+           return 3 * x;
+        }
+    }
+}
