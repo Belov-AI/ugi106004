95a6c0dd361008ec84e23c8d2ad5eb34ffe54b40
Belov-AI <Belov.Alexander@urfu.ru>
2020-11-23 13:57:31 +0500

    Добавлен проект Arrays

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers;
+
+            Console.WriteLine("Введите число элементов массива");
+
+            var n = int.Parse(Console.ReadLine());
+            numbers = new int[n];
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Введите элемент");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            PrintArray(numbers); 
+               
+
+            Console.ReadKey();
+        }
+
+        static void PrintArray(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+                Console.Write($"{array[i]} ");
+
+            Console.WriteLine();
+        }
+    }
+}
