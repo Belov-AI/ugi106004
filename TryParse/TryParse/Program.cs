7d078565f700a0aef79d7551e5ec055566459b97
Belov-AI <Belov.Alexander@urfu.ru>
2020-11-25 20:36:56 +0500

    Добавлен проект TryParse

---
+++ TryParse/TryParse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace TryParse
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число");
+
+            double number;
+
+            //var attempt = double.TryParse(Console.ReadLine(), out number);
+
+            //Console.WriteLine($"Удалась попытка? {attempt}\nЧисло равно {number}");
+
+            if (!double.TryParse(Console.ReadLine(), out number))
+                Console.WriteLine("Неправильный формат числа");
+            else
+                Console.WriteLine($"Число равно {number}");
+            
+            Console.ReadKey();
+        }
+    }
+}
