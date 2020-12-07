6d298bc02d33096c7192307649fe1720214aee92
Belov-AI <Belov.Alexander@urfu.ru>
2020-12-07 13:56:46 +0500

    В проект Arrays добавлены многомерные массивы

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -20,22 +20,87 @@ namespace Arrays
             }
 
             PrintArray(numbers);
+            Array.Sort(numbers);
+            PrintArray(numbers);
 
-            numbers = new[] { -5, -10 };
-               
+
+            PrintIndexOf(numbers, 2);
+            PrintIndexOf(numbers, 5);
+
+
+            //var newNumbers = (int[])numbers.Clone();
+
+            //PrintArray(newNumbers);
+            //newNumbers[0] = 111;
+            //PrintArray(newNumbers);
+            //PrintArray(numbers);
+
+            //newNumbers[1] = 222;
+            //newNumbers[2] = 333;
+            //newNumbers[3] = 444;
+            //newNumbers[4] = 555;
+            //Array.Copy(newNumbers, numbers, newNumbers.Length);
+            //PrintArray(numbers);
+
+            //double[] array = new double[n];
+            //Array.Copy(numbers, array, numbers.Length);
+            //PrintArray(array);
+
+            Console.WriteLine();
+
+            var matrix = new int[3, 4];
+
+            var rnd = new Random();
+
+            for (var i = 0; i < matrix.GetLength(0); i++)
+                for (var j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = rnd.Next(0, 100);
+
+            PrintMatrixArray(matrix);
 
             Console.ReadKey();
         }
 
         static void PrintArray(int[] array)
         {
-            //for (var i = 0; i < array.Length; i++)
-            //    Console.Write($"{array[i]} ");
-
             foreach(var elem in array)
-                Console.Write($"{elem} ");
+                Console.Write($"{elem}, ");
+
+            Console.Write("\b\b ");
 
             Console.WriteLine();
+        }
+
+        static void PrintArray(double[] array)
+        {
+            foreach (var elem in array)
+                Console.Write($"{elem}, ");
+
+            Console.Write("\b\b ");
+
+            Console.WriteLine();
+        }
+
+        static void PrintIndexOf(int[] array, int number)
+        {
+            var index = Array.BinarySearch(array, number);
+
+            if (index < 0)
+                Console.WriteLine($"Элемент \"{number}\" не найден");
+            else
+                Console.WriteLine($"Элемент \"{number}\" имеет индекс {index}");
+        }
+
+        static void PrintMatrixArray(int[,] matrix)
+        {
+            for (var i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (var j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write($"{matrix[i, j],2} ");
+
+                Console.WriteLine();
+            }
+                
         }
     }
 }
