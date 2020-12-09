3e385912db64f947a77d249c70b41e1fda4ba0a4
Belov-AI <Belov.Alexander@urfu.ru>
2020-12-09 16:05:53 +0500

    Добавлениы проекты вычисления простых чисел Софи Жермен и чисел Рекамана

---
+++ RecamanSequence/RecamanSequence/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace RecamanSequence
+{
+    class Program
+    {
+        static void Main()
+        {
+            int m;
+
+            while (true)
+            {
+                Console.WriteLine("Введите число членов последовательности");
+
+                if (!int.TryParse(Console.ReadLine(), out m))
+                {
+                    Console.WriteLine("Ошибка ввода\n");
+                    continue;
+                }
+
+                if (m <= 1)
+                {
+                    Console.WriteLine("Число членов последовательности должно быть положительным\n");
+                    continue;
+                }
+
+                break;
+            }
+
+            Console.WriteLine();
+
+            foreach(var elem in GetRecamanNumbers(m))
+                Console.Write($"{elem} ");
+
+            Console.ReadKey();
+        }
+
+        static int[] GetRecamanNumbers(int length)
+        {
+            int[] recamanNumbers = new int[length];
+
+            for (var i = 1; i < recamanNumbers.Length; i++)
+            {
+                var number = recamanNumbers[i - 1] - i;
+
+                if (number < 0)
+                    number = recamanNumbers[i - 1] + i;
+
+                if (IsInArray(recamanNumbers, number))
+                    number = recamanNumbers[i - 1] + i;
+
+                recamanNumbers[i] = number;
+            }
+
+            return recamanNumbers;
+        }
+
+        static bool IsInArray(int[] array, int number)
+        {
+            foreach(var elem in array)
+                if(elem == number)
+                    return true;
+
+            return false;
+        }
+    }
+}
---
+++ SophieGermainSequence/SophieGermainSequence/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace SophieGermainSequence
+{
+    class Program
+    {
+        static void Main()
+        {
+            int m;
+
+            while (true)
+            {
+                Console.WriteLine("Введите число членов последовательности");
+
+                if(!int.TryParse(Console.ReadLine(), out m))
+                {
+                    Console.WriteLine("Ошибка ввода\n");
+                    continue;
+                }
+
+                if(m <= 1)
+                {
+                    Console.WriteLine("Число членов последовательности должно быть положительным\n");
+                    continue;
+                }
+
+                break;
+            }
+
+            Console.WriteLine();
+
+            var current = 1;
+
+            for(var i = 0; i < m; i++)
+            {
+                current = GetNextSophieGermainPrimeNumber(current);
+                Console.Write($"{current} ");
+            }
+
+            Console.ReadKey();
+        }
+
+        static int GetNextSophieGermainPrimeNumber(int number)
+        {
+            while (true)
+            {
+                number++;
+                if (IsPrime(number) && IsPrime(2 * number + 1))
+                    return number;
+            }
+
+        }
+
+        static bool IsPrime(int number)
+        {
+            var result = true;
+            var bound = Math.Sqrt(number);
+
+            for (var d = 2; d <= bound; d++)
+            {
+                if(number % d == 0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
