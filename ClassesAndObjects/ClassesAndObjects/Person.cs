699fc99cce83567aa1894c0919e768f092b68618
Belov-AI <Belov.Alexander@urfu.ru>
2020-12-21 17:04:05 +0500

    Добавлен проект ClassesAndObjects

---
+++ ClassesAndObjects/ClassesAndObjects/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    public class Person
+    {
+        static public string Species = "Homo Sapience";
+
+        //int inn;
+
+        public string Name;
+        public string Surname;
+
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (value > 0)
+                    age = value;
+                else
+                    throw new ArgumentException("Возраст должен быть положительным");
+            }
+        }
+
+        public string GetFullName()
+        {
+            return $"{Name} {Surname}";
+        }
+
+        public void PrinInfo()
+        {
+            Console.WriteLine($"Имя: {Name}. Фамилия: {Surname}. Возраст: {Age}");
+        }
+
+        static public void PrintSpecies(Person person)
+        {
+            person.PrinInfo();
+            Console.WriteLine($"Биологический вид: {Species}");
+        }
+
+    }
+}
---
+++ ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main()
+        {
+            var peter = new Person();
+            peter.Name = "Пётр";
+            peter.Surname = "Иванов";
+            peter.Age = 17;
+
+            var ann = new Person { Name = "Анна", Surname = "Сергеева", Age = 17 };
+
+            Console.WriteLine(peter.GetFullName());
+            peter.PrinInfo();
+            ann.PrinInfo();
+
+            ann.Surname = peter.Surname + "a";
+            ann.Age = 20;
+            ann.PrinInfo();
+
+
+            Console.WriteLine();
+            Console.WriteLine(Person.Species);
+            Person.PrintSpecies(peter);
+
+            Console.ReadKey();
+        }
+    }
+}
