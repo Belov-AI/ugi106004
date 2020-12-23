40bc53da495b14c609a84dcbe6b12b9c87d0f91d
Belov-AI <Belov.Alexander@urfu.ru>
2020-12-23 17:14:03 +0500

    Решение заданий 9 и 10

---
+++ Task09/Task09/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+
+namespace Task09
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите строку текста");
+            var codes = CreateArray(Console.ReadLine());
+
+            PrintCodes(codes);
+
+            ChangeCodes(codes);
+            PrintCodes(codes);
+
+            Console.WriteLine(GetSomeString(codes));
+
+            Console.WriteLine("Введите натуральное число");
+            var k = int.Parse(Console.ReadLine());
+
+            PrintCodes(GetShiftedCodes(codes, k));
+
+            Console.ReadKey();
+
+        }
+
+        static int[] CreateArray(string text)
+        {
+            var result = new int[text.Length];
+
+            for (var i = 0; i < result.Length; i++)
+                result[i] = text[i];
+
+            return result;
+        }
+
+        static void PrintCodes(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+                if (i < array.Length - 1)
+                    Console.Write($"{ConvertToHexString(array[i])}, ");
+                else
+                    Console.Write($"{ConvertToHexString(array[i])}\n");
+        }
+
+        static void ChangeCodes(int[] array)
+        {
+            for(var i = 0; i < array.Length; i++)
+            {
+                var symbol = (char)array[i];
+
+                if (char.IsLetter(symbol))
+                    if (char.IsUpper(symbol))
+                        symbol = char.ToLower(symbol);
+                    else
+                        symbol = char.ToUpper(symbol);
+
+                array[i] = symbol;
+            }                
+        }
+
+        static string GetSomeString(int[] array)
+        {
+            string result = "";
+
+            foreach (var number in array)
+                if (number % 2 != 0)
+                    result = (char)number + result;
+
+            return result;
+        }
+
+        static int[] GetShiftedCodes(int[] array, int shift)
+        {
+            if (shift < 1)
+                throw new ArgumentException("Сдвиг должен быть положительным");
+
+            var result = new int[array.Length];
+
+            for (var i = 0; i < array.Length; i++)
+                if (array[i] % 2 == 0)
+                    result[i] = array[i] + shift;
+                else
+                    result[i] = array[i];
+
+            return result;
+        }
+
+        static string ConvertToHexString(int number)
+        {
+            return "0x" + Convert.ToString(number, 16).ToUpper();
+        }
+    }
+}
---
+++ Task10/Task10/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 0, n = 0;
+
+            var tryAgain = true;
+
+            while(tryAgain)
+            {
+                Console.WriteLine("Введите число строк (m)");
+                var input = Console.ReadLine();
+
+                if(int.TryParse(input, out m) && m >= 5 && m <= 20 )
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Введите число столбцов (n)");
+                        input = Console.ReadLine();
+
+                        if (int.TryParse(input, out n) && n >= 5 && n <= 20)
+                        {
+                            tryAgain = false;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка ввода\n");
+                            continue;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода\n");
+                    continue;
+                }
+            }
+
+            var rnd = new Random();
+
+            var table = new int[m, n];
+
+            for (var i = 0; i < table.GetLength(0); i++)
+                for (var j = 0; j < table.GetLength(1); j++)
+                    table[i, j] = rnd.Next(0, 100);
+
+            PrintTable(table);
+
+            var index = GetIndexOfOddColumn(table);
+
+            if (index < 1)
+                Console.WriteLine("Столбец не найден");
+            else
+                Console.WriteLine($"Индекс столбца из нечетных элементов: {index}.");
+
+            var averages = GetAverageByRow(table);
+
+            for (var i = 0; i < averages.Length; i++)
+                Console.WriteLine($"Строка: {i,2} Среднее: {averages[i]:F3}");
+
+            Console.ReadKey();
+        }
+
+        static void PrintTable(int[,] t)
+        {
+            for(var i = 0; i < t.GetLength(0); i++)
+            {
+                for (var j = 0; j < t.GetLength(1); j++)
+                    Console.Write($"{t[i, j], 2} ");
+
+                Console.WriteLine();
+            }
+        }
+
+        static int GetIndexOfOddColumn(int[,] t)
+        {
+            int j;
+
+            for(j = 0; j < t.GetLength(1); j++)
+            {
+                int i;
+
+                for(i = 0; i < t.GetLength(0); i++)
+                    if (t[i, j] % 2 == 0)
+                        break;
+
+                if (i == t.GetLength(0))
+                    break;
+            }
+
+            if (j == t.GetLength(1))
+                return -1;
+            else
+                return j;
+        }
+
+        static double[] GetAverageByRow(int[,] t)
+        {
+            var result = new double[t.GetLength(0)];
+
+            for(var i = 0; i < t.GetLength(0); i++)
+            {
+                var s = 0.0;
+
+                for (var j = 0; j < t.GetLength(1); j++)
+                    s += t[i, j];
+
+                result[i] = s / t.GetLength(1);
+            }
+
+            return result;
+        }
+    }
+}
